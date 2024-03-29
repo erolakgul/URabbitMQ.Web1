0aff43166642c6d4d96faa2446b0050b95dd1d6f
erol.akgul <erolakgul88@gmail.com>
2024-03-29 10:26:35 +0300

    Add project files.

---
+++ URabbitMQ.Web1/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URabbitMQ.Web1.Models;
+
+namespace URabbitMQ.Web1.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
